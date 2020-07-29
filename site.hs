e4b8d9b6cddd0c63648727b63bdfb74694b43c72
johnpaulwelsh <john.welsh1@marist.edu>
2020-07-28 21:09:12 -0400

    Adding take-15 to index list

--- site.hs
+++ site.hs
@@ -46,7 +46,7 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- fmap (take 15) . recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
