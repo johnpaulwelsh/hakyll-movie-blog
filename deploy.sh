528d4cb55bb4b140436df92ac55ee6003cbfa51a
johnpaulwelsh <john.welsh1@marist.edu>
2023-01-28 19:51:12 -0500

    fixing deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-# Temporarily store uncommited changes
-git stash
-
 # Verify correct branch
 git checkout master
 
@@ -8,21 +5,9 @@ git checkout master
 stack exec site clean
 stack exec site build
 
-# Get previous files
-git fetch --all
-git checkout -b gh-pages --track origin/gh-pages
-
-# Overwrite existing files with new files
-cp -a _site/. .
-
 # Commit
 git add -A
 git commit -m "New publish"
 
 # Push
-git push origin gh-pages:gh-pages
-
-# Restoration
-git checkout master
-git branch -D gh-pages
-git stash pop
+git push origin master:master
